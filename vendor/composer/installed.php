b7e1fb39ba62f573205e1b2c31a9430396b6c860
JoaoColonna <joao.colonna@outlook.com>
2024-03-25 17:46:09 -0300

    feat: Router, bootstrap and first exercise.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'php/projetocomposer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8f845eb1b48f40c33e74dfc5f1aa7eab7db75585',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'php/projetocomposer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8f845eb1b48f40c33e74dfc5f1aa7eab7db75585',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
